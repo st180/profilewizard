0eacbac0d3ddb7d2ba96e23184473787573302fe
Shaun Teeshad <shaun.teeshad@gmail.com>
2019-08-24 15:17:27 -0400

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PersonalDetailsComponent } from './personal-details/personal-details.component';
+import { ContactDetailsComponent } from './contact-details/contact-details.component';
+import { SkillsComponent } from './skills/skills.component';
+import { WorkExperienceComponent } from './work-experience/work-experience.component';
+import { WizardFinishedComponent } from './wizard-finished/wizard-finished.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "welcome", pathMatch: "full" },
+  { path: "welcome", component: WelcomeComponent },
+  { path: "personal-details", component: PersonalDetailsComponent },
+  { path: "contact-details", component: ContactDetailsComponent },
+  { path: "skills", component: SkillsComponent },
+  { path: "work-experience", component: WorkExperienceComponent },
+  { path: "wizard-finished", component: WizardFinishedComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PersonalDetailsComponent } from './personal-details/personal-details.component';
+import { ContactDetailsComponent } from './contact-details/contact-details.component';
+import { SkillsComponent } from './skills/skills.component';
+import { WorkExperienceComponent } from './work-experience/work-experience.component';
+import { WizardFinishedComponent } from './wizard-finished/wizard-finished.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonalDetailsComponent,
+    ContactDetailsComponent,
+    SkillsComponent,
+    WorkExperienceComponent,
+    WizardFinishedComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
